d1a3e1e40e959e11a324e8dbc9f3ace6dda249d3
Dana Hart <danahart@northwesternmutual.com>
2017-03-26 08:30:11 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict'
+var Alexa = require("alexa-sdk");
+var APP_ID = '';
+var facts = require('./intro-facts.json');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(startSessionHandlers, InfoCollectorHandlers);
+    alexa.execute();
+};
+
+var startSessionHandlers = {
+    'StartSession': function() {
+        this.emit(':askWithCard', 'Hello, ask for a Minecraft fact');
+    },
+    'AMAZON.StopIntent': function() {
+        this.emit(':tell', "Goodbye!");
+    },
+    'AMAZON.CancelIntent': function() {
+        this.emit(':tell', "Goodbye!");
+    },
+    'SessionEndedRequest': function() {
+        console.log('session ended!');
+        this.emit(":tell", "Goodbye!");
+    }
+}
+
+var InfoCollectorHandlers = {
+    'StartSession': function() {
+        this.emit('StartSession');
+    },
+    'TriviaIntent': function() {
+        var randomId = Math.floor(Math.random() * 15)
+        var randomFact = facts[randomId]
+        this.emit(':tell', randomFact.fact);
+    },
+    'AMAZON.CancelIntent': function() {
+        this.emit(':tell', "Goodbye!");
+    },
+    'SessionEndedRequest': function() {
+        console.log('session ended!');
+        this.emit(":tell", "Goodbye!");
+    },
+    'Unhandled': function() {
+        console.log("UNHANDLED");
+        this.emit(':ask', 'Sorry, I didn\'t get that. Try asking for minecraft trivia or fact.', 'Try asking for minecraft trivia or fact.');
+    }
+}
